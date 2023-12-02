1b7f0e069f0a6f556fa132da978767339160d0e5
Shiran <shiranaselasubasinghe@gmail.com>
2023-12-03 01:43:09 +0530

    Material UI

---
+++ Exercises/Material UI/sample/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import TourCard from './components/TourCard';
+import Container from '@mui/material/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <TourCard/>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
